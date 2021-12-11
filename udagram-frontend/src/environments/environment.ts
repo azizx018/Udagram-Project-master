41cdabbd96d0ae92c99de0ef6fa941bb1165ebd5
azizx018 <chiara.aziz@gmail.com>
2021-12-10 18:48:27 -0600

    updated start script

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://Udagramfinalproject-env.eba-w9fpbhzf.us-east-1.elasticbeanstalk.com '
 };
 
 /*

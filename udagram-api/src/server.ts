4553bb277d3a07995cbe7e01f6b18ec144cf9f4e
azizx018 <chiara.aziz@gmail.com>
2021-12-16 20:10:40 -0600

    updated port in server file

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -22,7 +22,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 5432;
 
   app.use(bodyParser.json());
 
